6e39257ea16da0f01749bf163e8d796462ffde90
“rskidmore1” <ryan.skidmore@yahoo.com>
2022-09-06 18:21:56 -0700

    Adding vehicle update PUT method.

--- index.js
+++ index.js
@@ -40,6 +40,44 @@ api.get('/vehicle', (req, res) => {
 
 })
 
+api.put('/vehicle', (req, res) => {
+
+  const sql = `SELECT * FROM vehicles WHERE idvehicles = ${req.query.idvehicles}`;
+  let idvehicles = req.query.idvehicles;
+  let make = req.query.make;
+  let model = req.query.model;
+  let color = req.query.color;
+  let year = req.query.year;
+
+  function flatten(input){
+    const toString = String(input)
+    const removeSpaces = toString.replace(/ /g, "");
+    const lowerCase = removeSpaces.toLowerCase();
+    return lowerCase;
+  }
+
+  pool.query(sql, function (err, result){
+    if (err) throw err;
+    result.map(field=>{
+      flatten(make) === flatten(field.make) || make === undefined ? make = field.make : null;
+      flatten(model) === flatten(field.model) || model === undefined ? model = field.model : null;
+      flatten(color) === flatten(field.color) || color === undefined ? color = field.color : null;
+      year === field.year || year === undefined ? make = field.year : null;
+
+    })
+
+  });
+
+const updateQuery = `UPDATE vehicles SET make = '${make}', model = '${model}', year = ${year}, color = '${color}' WHERE idvehicles = ${idvehicles};`
+
+  pool.query(updateQuery, function (err, result){
+    if (err) throw err;
+
+    res.send(result);
+  });
+
+})
+
 api.post('/newvehicle', (req, res) => {
 
   const sql = `INSERT INTO vehicles (make, model, color, year) values ("${req.query.make}", "${req.query.model}", "${req.query.color}", ${req.query.year})`;
